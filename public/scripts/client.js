85a1c9a9137931d0d557ebdd2d140cc14b09f22c
Christian Gravel <cjgravel@gmail.com>
2023-03-29 21:39:30 -0700

    created renderTweets and createTweetElement functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,69 @@
+const e = require("express");
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const renderTweets = function(tweets) {
+  const $tweetsContainer = $(".tweets");
+  $tweetsContainer.empty();
+  
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $tweetsContainer.append($tweet);
+  }
+}
+
+const createTweetElement = function(tweet) {
+let $tweet =  $(`
+  <article class="tweets__article">
+   <header class="tweets__header">
+     <div class="tweets__img__name">
+        <img src="${user.avatars}" alt="Profile-picture">
+        <name>${user.name}</name>
+     </div>
+        <span>${user.handle}</span>
+   </header>
+   <p class="content">${content.text}</p>
+   <div class="tweets__line"></div>
+    <footer class="tweets__footer">
+     <time datetime="${created_at}">${timeAgo}</time>
+     <span class="share__emojis">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-sharp fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </span>
+    </footer>
+  </article>`);
+
+return $tweet;
+}
+
+renderTweets(data);
