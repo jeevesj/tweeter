de0bc6b69d690fca7ea7b61cb3f23dade89935a8
Christian Gravel <cjgravel@gmail.com>
2023-03-31 15:32:30 -0700

    counter color changes by add and remove class

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,12 @@
-
 $(document).ready(function() {
   // --- our code goes here ---
-  $("#tweet-text").mouseover(function(){
-    $(this).css("background-color", "white");
-  });
-  $("#tweet-text").mouseout(function(){
-    $(this).css("background-color", "#f4f1ec");
-  })
   $("#tweet-text").on('input', function(){
     const remainingChars = 140 - $(this).val().length;
     $(".counter").text(remainingChars);
+    if(remainingChars < 0) {
+      $(".counter").removeClass('makeBlack').addClass('makeRed');
+    } else {
+      $(".counter").removeClass('makeRed').addClass('makeBlack');
+    }
   })
 });
