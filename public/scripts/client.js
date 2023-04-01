6deeee8f094ba7cbff7f49bbe68f6c895420f77b
Christian Gravel <cjgravel@gmail.com>
2023-03-31 17:11:41 -0700

    adding images to readme

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,12 +1,9 @@
 
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-*/
 
 const renderTweets = function(tweets) {
+  // empty the tweets so dont load multiples
   $(".tweets").empty();
+  // loop through the tweets calling the create tweet function and prepend it.
   for (const tweet of tweets) {
     const tweetElement = createTweetElement(tweet);
     $(".tweets").prepend(tweetElement);
@@ -14,7 +11,10 @@ const renderTweets = function(tweets) {
 }
 
 const createTweetElement = function(tweet) {
+
+  // Creating a variable 'time' to hold the formatted 'timeago'
   let time = timeago.format(tweet.created_at);
+
   const $content = $('<p class="content"></p>').text(tweet.content.text);  
   
   let $tweet =  $(`
@@ -39,16 +39,17 @@ const createTweetElement = function(tweet) {
   </article>
   <br>`
   );
+  // using .find and .after to inject this back into our html after the header
   $tweet.find('header').after($content);
 return $tweet;
 };
 
 
-
+// document ready function to ensure page is loaded before running.
 $(document).ready(function() {
-  //renderTweets(data);
 
   function loadTweets() {
+
     $.ajax({
       url: '/tweets',
       method: 'GET',
@@ -70,26 +71,40 @@ $(document).ready(function() {
 
   
   $form.on('submit', (event) => {
+    
+    // Preventing page refresh on form submission
     event.preventDefault();
 
     const textarea = $('#tweet-text');
     const textValue = textarea.val();
+
+    // Hiding error message so we can use slidedown later
     $("#error-message").hide();
     
+    // Error message if form is empty or null
     if (textValue === '' || textValue === null) {
+
       $("#error-message").text("Say something!").slideDown();
+
       return;
     }
     
+    // Error message counter is greater than 140 chars
     if (textValue.length > 140) {
+
       $("#error-message").text("Looks like you're over the limit!").slideDown();
+      
       return;
     }
 
+    // sliding message back up before submit going through
     $("#error-message").slideUp();
     console.log("forms been submitted");
+
+    // Urlencoding our text input and console logging to dom
     const urlencoded = $form.serialize();
     console.log(urlencoded);
+    
 
     $.ajax({
       url: '/tweets',
@@ -99,11 +114,15 @@ $(document).ready(function() {
         console.log('Form submitted successfully:', response);
         // Refresh the list of tweets after submitting the form
         loadTweets();
+        // Reset text area
         textarea.val('');
+        // Reset counter
         textarea.trigger('input');
       },
       error: function(error) {
+
         console.error('Error submitting form:', error);
+
       }
     });
   });
